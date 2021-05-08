de56eb62c8a55b7690ec42d21ca5f04532c13b5f
Max Heyer <mail@maxheyer.com>
2021-05-09 00:36:31 +0200

    Add yabai and skhd configs

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/nvim/lua/maxheyer
+mkdir -p ~/.config/skhd
+mkdir -p ~/.config/yabai
 
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
@@ -10,3 +12,6 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/.dotfiles/.config/nvim/lua/maxheyer/init.lua ~/.config/nvim/lua/maxheyer/init.lua
+
+ln -s ~/.dotfiles/.config/skhd/skhdrc ~/.config/skhd/skhdrc
+ln -s ~/.dotfiles/.config/yabai/yabairc ~/.config/yabai/yabairc
