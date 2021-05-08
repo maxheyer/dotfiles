4e78dfd885886c48b1f037a3f3b720407f15a14a
Max Heyer <mail@maxheyer.com>
2021-05-09 00:29:24 +0200

    Remove linux install

--- install.sh
+++ install.sh
@@ -1,20 +1,12 @@
-#!/bin/env bash
+#!/bin/bash
 
-mkdir -p ~/.i3
 mkdir -p ~/.config/alacritty
-mkdir -p ~/.config/rofi
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/nvim/lua/maxheyer
-mkdir -p ~/.config/polybar
 
-ln -s ~/.dotfiles/.i3/config ~/.i3/config
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 ln -s ~/.dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -s ~/.dotfiles/.config/rofi/config ~/.config/rofi/config
 ln -s ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/.dotfiles/.config/nvim/lua/maxheyer/init.lua ~/.config/nvim/lua/maxheyer/init.lua
-ln -s ~/.dotfiles/.config/polybar/config ~/.config/polybar/config
-ln -s ~/.dotfiles/.config/polybar/launch.sh ~/.config/polybar/launch.sh
-ln -s ~/.dotfiles/.config/picom.conf ~/.config/picom.conf
