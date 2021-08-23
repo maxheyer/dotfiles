79cc1dc1d68cdfa1ff0e83c9c6c3a8a45b4cf15a
Max Heyer <mail@maxheyer.com>
2021-08-23 16:59:34 +0200

    Change border chars

--- .config/nvim/lua/maxheyer/init.lua
+++ .config/nvim/lua/maxheyer/init.lua
@@ -95,16 +95,13 @@ require('telescope').setup{
     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
     winblend = 0,
     border = {},
-    borderchars = { '?', '?', '?', '?', '?', '?', '?', '?' },
     color_devicons = true,
+    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
     use_less = true,
     path_display = {},
     set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
     file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
     grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
     qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-
-    -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
   }
 }
