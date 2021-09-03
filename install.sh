e8128c9d2fc0d0f0221df8ce87385ba781aae2f0
Max Heyer <mail@maxheyer.com>
2021-09-03 17:19:16 +0200

    Fix install

--- install.sh
+++ install.sh
@@ -9,8 +9,7 @@ ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 ln -s ~/.dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -s ~/.dotfiles/.config/nvim/init.lua ~/.config/nvim/init.lua
-ln -s ~/.dotfiles/.config/nvim/lua ~/.config/nvim/lua
+ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
 
 ln -s ~/.dotfiles/.config/skhd/skhdrc ~/.config/skhd/skhdrc
 ln -s ~/.dotfiles/.config/yabai/yabairc ~/.config/yabai/yabairc
