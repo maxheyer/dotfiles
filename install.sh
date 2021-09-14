ccd9f17acf245311f165123882b872aea25ecce1
Max Heyer <mail@maxheyer.com>
2021-09-14 10:40:51 +0200

    Add tmux conf

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ mkdir -p ~/.config/yabai
 
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
 ln -s ~/.dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s ~/.dotfiles/.config/nvim/init.lua ~/.config/nvim/init.lua
