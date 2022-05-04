2c40e429cfd820816d964e80e6d0de3ab325a851
Max Heyer <max@wunder.software>
2022-05-04 13:05:50 +0200

    Add lightspeed config

---
+++ .config/nvim/lua/maxheyer/plugins/lightspeed.lua
@@ -0,0 +1,13 @@
+local function init()
+    vim.api.nvim_exec([[
+        autocmd ColorScheme * lua require'lightspeed'.init_highlight(true)
+        nmap <expr> f reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_f" : "f"
+        nmap <expr> F reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_F" : "F"
+        nmap <expr> t reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_t" : "t"
+        nmap <expr> T reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_T" : "T"
+    ]], false)
+end
+
+return {
+  init = init
+}
