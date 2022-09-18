74c4f5c0914ac2e267a8c6ff74a1fd2c782b7e7c
Max Heyer <max@wunder.software>
2022-09-18 11:49:09 +0200

    Format code

--- .config/nvim/lua/maxheyer/packer.lua
+++ .config/nvim/lua/maxheyer/packer.lua
@@ -4,48 +4,56 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require("packer").startup(function(use)
-    -- Packer can manage itself
-    use("wbthomason/packer.nvim")
+  -- Packer can manage itself
+  use("wbthomason/packer.nvim")
 
-    -- Icons
-    use("kyazdani42/nvim-web-devicons")
-    use("ryanoasis/vim-devicons")
+  -- Icons
+  use("kyazdani42/nvim-web-devicons")
+  use("ryanoasis/vim-devicons")
 
-    -- LSP
-    use("neovim/nvim-lspconfig")
-    use("onsails/lspkind-nvim")
-    use("nvim-lua/lsp_extensions.nvim")
-    use("glepnir/lspsaga.nvim")
-    use("j-hui/fidget.nvim")
-    use("ray-x/lsp_signature.nvim")
+  -- LSP
+  use("neovim/nvim-lspconfig")
+  use("onsails/lspkind-nvim")
+  use("nvim-lua/lsp_extensions.nvim")
+  use("glepnir/lspsaga.nvim")
+  use("j-hui/fidget.nvim")
+  use("ray-x/lsp_signature.nvim")
 
-    -- Telescope
-    use("nvim-lua/plenary.nvim")
-    use("nvim-lua/popup.nvim")
-    use("nvim-telescope/telescope-file-browser.nvim")
-    use("nvim-telescope/telescope-fzy-native.nvim")
-    use("nvim-telescope/telescope.nvim")
+  -- Telescope
+  use("nvim-lua/plenary.nvim")
+  use("nvim-lua/popup.nvim")
+  use("nvim-telescope/telescope-file-browser.nvim")
+  use{"nvim-telescope/telescope-fzf-native.nvim", run = 'make'}
+  use("nvim-telescope/telescope.nvim")
 
-    -- Treesitter
-    use("nvim-treesitter/nvim-treesitter", {
-        run = ":TSUpdate"
-    })
+  -- Treesitter
+  use("nvim-treesitter/nvim-treesitter", {
+    run = ":TSUpdate"
+  })
 
-    -- Completion
-    use("L3MON4D3/LuaSnip")
-    use("rafamadriz/friendly-snippets")
-    use("saadparwaiz1/cmp_luasnip")
-    use("hrsh7th/cmp-nvim-lsp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/nvim-cmp")
+  -- Completion
+  use("L3MON4D3/LuaSnip")
+  use("rafamadriz/friendly-snippets")
+  use("saadparwaiz1/cmp_luasnip")
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/nvim-cmp")
 
-    -- Colorscheme
-    use("catppuccin/nvim")
+  -- Colorscheme
+  use("tjdevries/colorbuddy.nvim")
+  use("lalitmee/cobalt2.nvim")
+  use("bbenzikry/snazzybuddy.nvim")
+  use("svrana/neosolarized.nvim")
+  use("catppuccin/nvim")
 
-    -- Others
-    use("lewis6991/gitsigns.nvim")
-    use("simrat39/symbols-outline.nvim")
-    use("akinsho/toggleterm.nvim")
-    use("editorconfig/editorconfig-vim")
-    use("TimUntersberger/neogit")
+
+  -- Status line
+  use("nvim-lualine/lualine.nvim")
+
+  -- Others
+  use("lewis6991/gitsigns.nvim")
+  use("simrat39/symbols-outline.nvim")
+  use("akinsho/toggleterm.nvim")
+  use("editorconfig/editorconfig-vim")
+  use("TimUntersberger/neogit")
 end)
