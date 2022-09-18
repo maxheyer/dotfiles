fe159785b19551dbf3405f888dcdf8e16e0991f1
Max Heyer <max@wunder.software>
2022-09-18 12:28:34 +0200

    Update vim config

--- .config/nvim/lua/maxheyer/set.lua
+++ .config/nvim/lua/maxheyer/set.lua
@@ -6,18 +6,21 @@ vim.opt.relativenumber = true
 
 vim.opt.errorbells = false
 
+vim.opt.title = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.autoindent = true
+
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-vim.opt.backup = false
 
 vim.opt.undofile = true
 vim.opt.swapfile = false
+vim.opt.backup = false
 
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
@@ -36,3 +39,9 @@ vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
 vim.cmd('set clipboard+=unnamedplus')
+
+vim.opt.cursorline = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = 'pum'
+vim.opt.pumblend = 5
+vim.opt.background = 'dark'
