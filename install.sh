d5bc8f13c6c72b6f4adef390f19caaaff7a49b63
Max Heyer <max@wunder.software>
2024-01-21 04:04:42 +0100

    Fix install

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/nvim
 
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
-ln -s ~/.dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s ~/.dotfiles/.config/alacritty ~/.config/alacritty
 ln -s ~/.dotfiles/.config/nvim/init.lua ~/.config/nvim/init.lua
 ln -s ~/.dotfiles/.config/nvim/lua ~/.config/nvim/lua
 ln -s ~/.dotfiles/.config/phpactor ~/.config/phpactor
