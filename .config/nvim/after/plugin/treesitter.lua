aec51c35a9994afe39d8df46a2ce22d32690f35c
Max Heyer <max@wunder.software>
2024-02-23 17:05:03 +0100

    Add tree-sitter-blade

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,15 @@
 require("nvim-treesitter.install").prefer_git = true
 
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.blade = {
+  install_info = {
+    url = "https://github.com/EmranMR/tree-sitter-blade",
+    files = {"src/parser.c"},
+    branch = "main",
+  },
+  filetype = "blade"
+}
+
 require'nvim-treesitter.configs'.setup{
   prefer_git = true,
   ensure_installed = "all",
@@ -19,3 +29,10 @@ require'nvim-treesitter.configs'.setup{
     enable = true
   }
 }
+
+local blade_group = vim.api.nvim_create_augroup("BladeFiltypeRelated", { clear = true })
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+  pattern = "*.blade.php",
+  group = blade_group,
+  command = "set ft=blade"
+})
