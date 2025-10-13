ea34c38b5bc94ac1cf2408f6d53a9d923f21d1bd
Max Heyer <mail@max.gp>
2025-10-13 03:23:14 +0200

    Fix ashen

--- configs/nvim/lua/plugins/example.lua
+++ configs/nvim/lua/plugins/example.lua
@@ -173,21 +173,7 @@ return {
       },
     },
   },
-  {
-  "ficcdaf/ashen.nvim",
-  -- optional but recommended,
-  -- pin to the latest stable release:
-  tag = "*",
-  lazy = false,
-  priority = 1000,
-  -- configuration is optional!
-  opts = {
-      transparent = true,
-      plugins = {
-        autoload = true,
-      },
-  },
-},
+  { "ficcdaf/ashen.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
